79761d5e98696187769764d18adbb0db2796e22a
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2023-12-25 19:16:35 +0800

    12.25

--- src/components/Home Page/PictureSlidePart.jsx
+++ src/components/Home Page/PictureSlidePart.jsx
@@ -7,21 +7,28 @@ export const PictureSlidePart = () => {
       .then(resp => resp.json())
       .then(articles => setArticles(articles))
   }, [])
-  console.log(articles)
   return (
-    <div className='mt-[100px] flex items-center flex-col'>
-      {articles.map((article) => {
-        return <div>
-          <div>
-            <img src={article.cover_image} alt="" />
-            <div className='bg-white w-[558px] h-[232px] relative bottom-60 left-2 p-10 rounded-lg'>
-              <div className='py-1 px-[10px] text-white text-base bg-[#4B6BFB]'>{article.tags}</div>
-              <h1 className='text-4xl font-semibold'>{article.title}</h1>
-              <p className='text-gray-500 mt-6'>{new Date(article.published_at).toLocaleDateString()}</p>
+    <div>
+      <div className='mt-[100px] flex items-center flex-col'>
+        {articles.map((article) => {
+          return <div>
+            <div>
+              <div className='relative'>
+                <img className='rounded-lg' src={article.cover_image} alt="" />
+              </div>
+              <div className='bg-white w-[558px] h-[232px] relative bottom-60 left-2 p-10 rounded-lg flex flex-col gap-4'>
+                <div className='py-1 px-[10px] text-white text-base bg-[#4B6BFB] rounded-lg'>{article.tags}</div>
+                <h1 className='font-semibold text-3xl'>{article.title}</h1>
+                <p className='text-gray-500'>{new Date(article.published_at).toLocaleDateString()}</p>
+              </div>
             </div>
           </div>
-        </div>
-      })}
+        })}
+      </div>
+      <div>
+        <button>asdnasdas</button>
+        <button>asdasdasd</button>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { HeaderPart } from '../components/HeaderPart'
 import { AllBlogPosts } from '@/components/Blog Page/AllBlogPosts'
 import { Footer } from '@/components/Footer'
 import { PictureSlidePart } from '@/components/Home Page/PictureSlidePart'
+import { Slide } from '@/components/Home Page/Slide'
+import { Data } from '@/components/Home Page/Data'
 
 
 
@@ -13,9 +15,10 @@ export default function Home() {
   return (
     <div className="lg:w-[1216px] m-auto">
       <HeaderPart />
-      <PictureSlidePart />
+      {/* <PictureSlidePart /> */}
       {/* <AllBlogPosts /> */}
-
+      {<Slide />}
+      {/* {<Data />} */}
     </div>
   )
 }
