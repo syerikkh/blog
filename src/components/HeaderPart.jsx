00ea456b02de1a7047e69b0368dd8083a02c5c33
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-01-09 19:32:11 +0800

    01.09

--- src/components/HeaderPart.jsx
+++ src/components/HeaderPart.jsx
@@ -2,13 +2,16 @@
 import { MetaBlogLogo } from './MetaBlogLogo'
 import { SearchIcon } from './SearchIcon'
 import Link from 'next/link'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { SearchContextValue } from '@/context/SearchCont'
 import { BurgerMenuIcon } from './BurgerMenuIcon'
+import { XIcon } from './XIcon'
+import { RightArrow } from './RightArrow'
 // import Blog from '@/pages/blog'
 
 export const HeaderPart = () => {
     const { searchValue, setSearchValue } = useContext(SearchContextValue);
+    const [active, setActive] = useState(false);
     return (
         <div className="lg:py-8 lg:w-full p-5">
             <div className="flex justify-between">
@@ -22,8 +25,8 @@ export const HeaderPart = () => {
                     <input id='search' className='pl-4 py-2 pr-2 bg-[#F4F4F5] rounded-md' type="text" placeholder='Search' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
                     <button><SearchIcon /></button>
                 </div>
-                <div className='block lg:hidden'>
-                    <BurgerMenuIcon />
+                <div className='block lg:hidden' onClick={() => setActive(!active)}>
+                    {active ? <RightArrow /> : <BurgerMenuIcon />}
                 </div>
             </div>
         </div >
---
+++ src/components/XIcon.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const XIcon = () => {
+    return (
+        <div><img src="blog/public/x-symbol-svgrepo-com.svg" alt="" /></div>
+    )
+}
